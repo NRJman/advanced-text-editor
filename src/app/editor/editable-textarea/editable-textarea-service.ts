8b84cbcc8da2c61058e3f91bd9fe8694159d2529
Vadym Hrynishyn <vadym.hrynishyn@avid.com>
2019-08-06 16:55:47 +0300

    fix a bug with redundant line breaks

--- src/app/editor/editable-textarea/editable-textarea-service.ts
+++ src/app/editor/editable-textarea/editable-textarea-service.ts
@@ -4,35 +4,36 @@ export type ProcessNodeReturnType = ChildNode | ChildNode[] | HTMLElement | HTML
 
 @Injectable()
 export class EditableTextareaService {
-    constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2) { }
 
-    public processNode(node: ChildNode): ProcessNodeReturnType  {
-      const nodeName = node.nodeName.toLowerCase();
-      const textContentLength: number = node.textContent.length;
+  public processNode(node: ChildNode): ProcessNodeReturnType  {
+    const nodeName = node.nodeName.toLowerCase();
+    const textContentLength: number = node.textContent.length;
+    const possibleNodeTags = ['span', 'br', '#text'];
 
-      if (nodeName === 'span' || nodeName === '#text') {
-        if (textContentLength > 0 && !(textContentLength === 1 && nodeName === 'span')) {
-          return node.textContent.split('').map((char: string): HTMLElement => {
-            let newNode: HTMLElement;
+    if (~possibleNodeTags.indexOf(nodeName)) {
+      if (textContentLength > 0 && !(textContentLength === 1 && nodeName === 'span')) {
+        return node.textContent.split('').map((char: string): HTMLElement => {
+          let newNode: HTMLElement;
 
-            newNode = this.renderer.createElement('span');
-            this.renderer.setProperty(newNode, 'innerText', char);
+          newNode = this.renderer.createElement('span');
+          this.renderer.setProperty(newNode, 'innerText', char);
 
-            return newNode;
-          });
-        }
-
-        return node;
+          return newNode;
+        });
       }
 
-      return this.getNewLineBreak();
+      return node;
     }
 
-    getNewLineBreak(): HTMLElement[] {
-      const elementToBeEditedFurther: HTMLElement = this.renderer.createElement('span');
+    return this.getNewLineBreak();
+  }
 
-      this.renderer.setProperty(elementToBeEditedFurther, 'innerText', '\n');
+  getNewLineBreak(): HTMLElement[] {
+    const elementToBeEditedFurther: HTMLElement = this.renderer.createElement('span');
 
-      return [this.renderer.createElement('br'), elementToBeEditedFurther];
-    }
+    this.renderer.setProperty(elementToBeEditedFurther, 'innerText', '\n');
+
+    return [this.renderer.createElement('br'), elementToBeEditedFurther];
+  }
 }
